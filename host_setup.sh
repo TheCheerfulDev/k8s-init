7a69e7e4de234345fd06eb29beb2586a3d114f62
Mark Hendriks <markhendriks85@gmail.com>
2021-04-09 21:22:44 +0200

    Node setup

--- host_setup.sh
+++ host_setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ $# -lt 2 ]; then
+    echo "<NODE NAME> <LAST NUMBER OF IP>"
+    exit 1
+fi
 sudo sed -i "s,base,$1,g" /etc/hosts
 sudo sed -i "s,base,$1,g" /etc/hostname
 sudo sed -i "s,192.168.178.199,192.168.178.20$2,g" /etc/netplan/00-installer-config.yaml
