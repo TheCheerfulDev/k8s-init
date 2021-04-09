18b9d3b00bba637c9aa7b9a059ef1900928b4af0
Mark Hendriks <markhendriks85@gmail.com>
2021-04-09 21:32:02 +0200

    revert newer docker

--- init_master.sh
+++ init_master.sh
@@ -30,13 +30,7 @@ echo
 echo "Install Docker"
 sleep 3
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-
+sudo apt-get install -y docker.io
 echo
 echo "Install kubeadm, kubelet, and kubectl"
 sleep 3
@@ -84,7 +78,7 @@ echo "Apply Calico network plugin from ProjectCalico.org"
 echo "If you see an error they may have updated the yaml file"
 echo "Use a browser, navigate to the site and find the updated file"
 
-kubectl apply -f calico.yaml
+kubectl apply -f calico.yml
 
 echo
 echo
