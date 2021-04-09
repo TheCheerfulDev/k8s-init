2016b4c511324c3bc302fe7de4f741d90d7292ab
Mark Hendriks <markhendriks85@gmail.com>
2021-04-09 21:18:33 +0200

    Node setup

--- host_setup.sh
+++ host_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo sed -i "s,base,$1,g" /etc/hosts
-sudo sed "s,base,$1,g" /etc/hostname
+sudo sed -i "s,base,$1,g" /etc/hostname
 sudo sed -i "s,192.168.178.199,192.168.178.20$2,g" /etc/netplan/00-installer-config.yaml
 sudo netplan apply
