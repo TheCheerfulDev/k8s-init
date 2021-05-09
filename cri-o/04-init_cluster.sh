3adb52c1fecb4e3e2417465ee5c7e92b955718d5
thecheerfuldev <markhendriks85@gmail.com>
2021-05-09 07:41:35 +0200

    ip fix

--- cri-o/04-init_cluster.sh
+++ cri-o/04-init_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo kubeadm init --pod-network-cidr 192.10.0.0/16 --apiserver-advertise-address 192.168.178.199 | tee ~/kubeadm-init.log
+sudo kubeadm init --pod-network-cidr 192.10.0.0/16 --apiserver-advertise-address 192.168.178.200 | tee ~/kubeadm-init.log
