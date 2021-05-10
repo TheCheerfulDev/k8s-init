8d74eec7995885f397733952781cfcf3f8f6d5bf
thecheerfuldev <markhendriks85@gmail.com>
2021-05-10 08:16:42 +0200

    cleaned up files

--- cri-o/04-init_cluster.sh
+++ cri-o/04-init_cluster.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 sudo kubeadm init --pod-network-cidr 192.10.0.0/16 --apiserver-advertise-address 192.168.178.200 | tee ~/kubeadm-init.log
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
